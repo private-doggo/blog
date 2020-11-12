acf2b7515a18e03bc6581033b04bc8c3bd9f778c
private-doggo <github.penguin.anonaddy.com>
2020-11-12 18:14:36 +0300

    ADDED CONNECTION TO SQL DATABASE

--- coursework/src/main/java/by/grsu/coursework/Controllers/BlogController.java
+++ coursework/src/main/java/by/grsu/coursework/Controllers/BlogController.java
@@ -1,5 +1,8 @@
 package by.grsu.coursework.Controllers;
 
+import by.grsu.coursework.models.Post;
+import by.grsu.coursework.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         model.addAttribute("title", "Блог");
         return "blog-main";
     }
