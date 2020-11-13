f95de66f5224457270943cc64fd6497a14510b2f
private-doggo <github.penguin.anonaddy.com>
2020-11-13 20:17:12 +0300

    Error handling implemented

---
+++ coursework/src/main/java/by/grsu/coursework/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package by.grsu.coursework.repo;
+
+import by.grsu.coursework.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+}
